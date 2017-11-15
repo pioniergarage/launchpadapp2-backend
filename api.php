dd1b48d44fa8792588153e60de4fd5391c00309f
m-wessendorf <max.wessendorf@t-online.de>
2017-11-16 00:55:57 +0100

    api.php main frame, setting up medoo database and slim app

--- api.php
+++ api.php
@@ -4,5 +4,36 @@
 * Version: 0.0.1
 */
 
+require 'vendor/autoload.php';
+include 'config.php';
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
+
+// Using Medoo namespace
+use Medoo\Medoo;
+
+// START of queries:
+
+
+// END of queries.
+
+$app->run();
+
+function db() {
+    return new Medoo([
+        'database_type' => $database_type,
+        'database_name' => $database_name,
+        'server' => $server,
+        'username' => $username,
+        'password' => $password
+    ]);
+}
 
 ?>
