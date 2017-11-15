8ea7644122b7b4d79bd7d1bfbf23d3bd11cf3cb3
m-wessendorf <max.wessendorf@t-online.de>
2017-11-16 00:48:53 +0100

    ignoring config.php and delivering example.config.php

---
+++ example.config.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* Author: Maximilian Wessendorf
+*/
+
+//Database setup variables.
+$database_type = 'mysql';
+$database_name = 'launchpadapp';
+$server = ''; // probably localhost
+$username = '';
+$password = '';
+
+?>
