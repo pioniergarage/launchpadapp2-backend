c7f6cd2850b0a9d77303812dc0f0a31b834d2d4c
m-wessendorf <max.wessendorf@t-online.de>
2017-11-17 17:50:52 +0100

    fixed integration of config file for database + added GET call whether open or closed

--- api.php
+++ api.php
@@ -5,7 +5,6 @@
 */
 
 require 'vendor/autoload.php';
-include 'config.php';
 
 $configuration = [
     'settings' => [
@@ -21,12 +20,24 @@ use Medoo\Medoo;
 
 // START of queries:
 
+// Check whether opened or closed.
+// Return "0" when closed and "1" when opened.
+$app->get('/ampelstate', function ($request, $response, $args) {
+    $data = db()->query("SELECT * FROM opening_times ORDER BY id DESC LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+    if ($data["close_at"] != null) {
+        echo json_encode("0");
+    } else if ($data["close_at"] == null) {
+        echo json_encode("1");
+    }
+    
+});
 
 // END of queries.
 
 $app->run();
 
 function db() {
+    require 'config.php';
     return new Medoo([
         'database_type' => $database_type,
         'database_name' => $database_name,
