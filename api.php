8b2dcb5e0a84fc63afa683a94cf329eea8a645fa
m-wessendorf <max.wessendorf@t-online.de>
2017-11-17 23:04:11 +0100

    added request to trigger change on the open/close state

--- api.php
+++ api.php
@@ -32,6 +32,22 @@ $app->get('/ampelstate', function ($request, $response, $args) {
     
 });
 
+// Trigger change of the opened/closed state. Max. execution rate is 5sec.
+// Return "successto0" when closed and "successto1" when opened.
+$app->get('/ampelchange', function ($request, $response, $args) {
+    // Get Last entry.
+    $lastEntry = db()->query("SELECT * FROM opening_times ORDER BY id DESC LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+    if ($lastEntry["close_at"] == null) {
+        // When last entry is open -> close it and return successto0
+        db()->query("UPDATE opening_times SET close_at = CURRENT_TIMESTAMP WHERE id = :id", [":id" => $lastEntry["id"]]);
+        echo json_encode("successto0");
+    } else if ($lastEntry["close_at"] != null) {
+        // When last entry is closed -> open new entry and return successto1
+        db()->query("INSERT INTO opening_times (open_at) VALUES (CURRENT_TIMESTAMP)");
+        echo json_encode("successto1");
+    }
+});
+
 // END of queries.
 
 $app->run();
