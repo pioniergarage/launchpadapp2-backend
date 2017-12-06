215600455800e6f7216916361af42181cbb79c3c
m-wessendorf <max.wessendorf@t-online.de>
2017-12-06 20:10:19 +0100

    Add Access Control

--- api.php
+++ api.php
@@ -18,6 +18,19 @@ $app = new \Slim\App($c);
 // Using Medoo namespace
 use Medoo\Medoo;
 
+// Add Acces-Control-Allow-Origin
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // START of queries:
 
 // Check whether opened or closed.
